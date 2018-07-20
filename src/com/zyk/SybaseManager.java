4a09f797bcef010b2476645cd614dceba2813c68
asdalun <dalun@yeah.net>
2018-07-20 13:12:10 +0800

    将Sybase操作类独立，每次插入建立连接，插入后关闭

--- src/com/zyk/SybaseManager.java
+++ src/com/zyk/SybaseManager.java
@@ -1,23 +1,65 @@
 package com.zyk;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
 import java.sql.ResultSet;
+import java.sql.Statement;
 
 import org.apache.log4j.Logger;
 
-public class SybaseManager extends DBManager {
+public class SybaseManager {
+	/**
+	 * 数据库名称（类型）比如Oracle, SQLServer等
+	 */
+	private String DBName = "";
 	/**
 	 * 缓存日志
 	 */
 	private static Logger logger = Logger.getLogger(SybaseManager.class);
+	/**
+	 * 驱动名称
+	 */
+	String DriverName = "";
+	/**
+	 * 数据库连接串
+	 */
+	String ConnectString = "";
+	/**
+	 * 数据库用户名
+	 */
+	String User = "";
+	/**
+	 * 数据库密码
+	 */
+	String Password = "";
 	
 	public SybaseManager() {
-		super("Sybase", logger);
-		super.setDriveName("com.sybase.jdbc2.jdbc.SybDriver");
-		super.setConnectString("jdbc:sybase:Tds:10.101.10.142:4100/alerts?charset=cp936");
-		super.setUser("root");
-		super.setPassword("");
+		this.DBName = "Sybase";
+		this.DriverName = "com.sybase.jdbc2.jdbc.SybDriver";
+		this.ConnectString = "jdbc:sybase:Tds:10.101.10.142:4100/alerts?charset=cp936";
+		this.User = "root";
+		this.Password = "";
 	}
-
+	/**
+	 * 连接数据库；返回连接器
+	 */
+	public Connection conncetToDB() {
+		try {
+			Class.forName(this.DriverName);
+			Connection conn = DriverManager.getConnection(this.ConnectString,
+	                        this.User, this.Password);
+	        return conn;
+		}
+		catch (Exception ex) {
+			logger.error("连接" + this.DBName + "失败：" + ex.toString());
+			return null;
+		}
+	}
+	/**
+	 * 插入数据到Sybase
+	 * @param rs  查询出的数据集
+	 * @return    成功 true 失败 false
+	 */
 	public boolean insertTivoliData(ResultSet rs) {
 		try {
 			String wl = "insert into alerts.status(Severity, Node, Summary, Identifier, FirstOccurrence, LastOccurrence, BUSINESSNAME, BUSINESSID, "
@@ -27,14 +69,17 @@ public class SybaseManager extends DBManager {
 					rs.getString("DEVICETYPE") + "', '" + rs.getString("ITMSITTYPE") + "', '" + rs.getString("SUMMARY_ORI") + 
 					"', '" + rs.getString("ALERTKEY") + "', '" + rs.getString("IP") + "')";
 			
-			if (this.updateData(wl)) {
-				logger.info("插入数据：" + wl);
-				return true;
-			}
-			else {
-				logger.error("插入数据错误：" + wl);
+			Connection conn = conncetToDB();
+			if (conn == null) {
+				logger.error("连接" + this.DBName + "失败!");
 				return false;
 			}
+			Statement stmt = conn.createStatement();
+			stmt.executeUpdate(wl);
+			stmt.close();
+			conn.close();
+			logger.info("添加数据：" + wl);
+			return true;
 		}
 		catch (Exception ex) {
 			logger.error("插入数据错误：" + ex.toString());
